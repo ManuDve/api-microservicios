61cd86a2285dc820e0c2a89804c30c7aa2e2fb02
ManuDv <m.alfaro@live.cl>
2023-10-13 19:37:56 -0300

    Habilita el CORS para correr la aplicacion de Angular

--- springboot-servicio-organizaciones/src/main/java/com/eureka/springboot/app/organizaciones/controllers/OrganizacionController.java
+++ springboot-servicio-organizaciones/src/main/java/com/eureka/springboot/app/organizaciones/controllers/OrganizacionController.java
@@ -3,6 +3,7 @@ package com.eureka.springboot.app.organizaciones.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ public class OrganizacionController {
 	private IOrganizacionService organizacionService;
 	
 	@GetMapping
+	@CrossOrigin
 	public List<Organizacion> getAllOrganizaciones() {
 		return organizacionService.getAllOrganizaciones();
 	}
